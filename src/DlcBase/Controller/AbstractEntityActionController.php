02159c3de424e18b81047c5a790677d85f5a5af5
Daniel Labas <daniel@dlabas.de>
2013-04-24 15:05:39 +0200

    Fix bug composer.json and add options to ViewModel

--- src/DlcBase/Controller/AbstractEntityActionController.php
+++ src/DlcBase/Controller/AbstractEntityActionController.php
@@ -12,25 +12,25 @@ abstract class AbstractEntityActionController extends AbstractActionController
 {
     /**
      * Index action for viewing a pagination list
-     * 
+     *
      * @return ViewModel
      */
     public function indexAction()
     {
         $query = (string) $this->params()->fromQuery('query', null);
         $query = strlen($query) > 0 ? $query : null;
-        
+
         $orderBy = (string) $this->params()->fromQuery('orderBy', null);
         $orderBy = strlen($orderBy) > 0 ? $orderBy : null;
-        
+
         $sort = (string) $this->params()->fromQuery('sort', null);
         $sort = strlen($sort) > 0 ? $sort : null;
-        
+
         $page  = (int) $this->params()->fromRoute('page', 1);
         $limit = $this->getOptions()->getDefaultItemsPerPage();
-        
+
         $entities = $this->getService()->pagination($page, $limit, $query, $orderBy, $sort);
-        
+
         $view = new ViewModel(array(
             'options'  => $this->getOptions(),
             'entities' => $entities,
@@ -38,13 +38,13 @@ abstract class AbstractEntityActionController extends AbstractActionController
             'orderBy'  => $orderBy,
             'sort'     => $sort,
         ));
-        
+
         return $view;
     }
-    
+
     /**
      * List action for viewing a pagination list
-     * 
+     *
      * @return ViewModel
      */
     public function listAction()
@@ -52,27 +52,28 @@ abstract class AbstractEntityActionController extends AbstractActionController
         $page  = (int) $this->params()->fromRoute('page', 1);
         return $this->forward()->dispatch($this->getRouteIdentifierPrefix(), array('action' => 'index', 'page' => $page));
     }
-    
+
     /**
      * Show action for viewing a single entity
-     * 
+     *
      * @return \Zend\View\Model\ViewModel
      */
     public function showAction()
     {
         $id = (int) $this->params()->fromRoute('id', null);
-        
+
         $service = $this->getService();
         $entity  = $service->getById($id);
-        
+
         return new ViewModel(array(
-            'entity' => $entity
+            'entity'  => $entity,
+            'options' => $this->getOptions(),
         ));
     }
-    
+
     /**
      * Add action for adding a new entity
-     * 
+     *
      * @return ViewModel
      */
     public function addAction()
@@ -90,13 +91,14 @@ abstract class AbstractEntityActionController extends AbstractActionController
 
         $redirectUrl = $this->url()->fromRoute($this->getAddActionRoute())
                      . ($redirect ? '?redirect=' . $redirect : '');
-        
+
         $prg = $this->prg($redirectUrl, true);
 
         if ($prg instanceof Response) {
             return $prg;
         } elseif ($prg === false) {
             return array(
+                'options'  => $this->getOptions(),
                 'form'     => $form,
                 'redirect' => $redirect,
             );
@@ -109,6 +111,7 @@ abstract class AbstractEntityActionController extends AbstractActionController
 
         if (!$entity) {
             return array(
+                'options'  => $this->getOptions(),
                 'form'     => $form,
                 'redirect' => $redirect,
             );
@@ -117,82 +120,84 @@ abstract class AbstractEntityActionController extends AbstractActionController
         // TODO: Add the redirect parameter here...
         return $this->redirect()->toUrl($this->url()->fromRoute($this->getRouteIdentifierPrefix()) . ($redirect ? '?redirect='.$redirect : ''));
     }
-    
+
     /**
      * Edit action for editing an entity
-     * 
+     *
      * @return ViewModel
      */
     public function editAction()
     {
         $id = (int) $this->params()->fromRoute('id', null);
-        
+
         $request = $this->getRequest();
         $service = $this->getService();
         $entity  = $service->getById($id);
-        
+
         $form     = $this->getEditForm();
         $form->bind($entity);
-        
+
         if ($request->getQuery()->get('redirect')) {
             $redirect = $request->getQuery()->get('redirect');
         } else {
             $redirect = false;
         }
-        
+
         $redirectUrl = $this->url()->fromRoute($this->getEditActionRoute(), array('id' => $id)) . ($redirect ? '?redirect=' . $redirect : '');
         $prg = $this->prg($redirectUrl, true);
-        
+
         if ($prg instanceof Response) {
             return $prg;
         } elseif ($prg === false) {
             return array(
+                'options'  => $this->getOptions(),
                 'id'       => $id,
                 'form'     => $form,
                 'redirect' => $redirect,
             );
         }
-        
+
         $post   = $prg;
         $entity = $service->update($id, $post);
-        
+
         $redirect = isset($prg['redirect']) ? $prg['redirect'] : null;
-        
+
         if (!$entity) {
             return array(
+                'options'  => $this->getOptions(),
                 'id'       => $id,
                 'form'     => $form,
                 'redirect' => $redirect,
             );
         }
-        
+
         return $this->redirect()->toUrl($this->url()->fromRoute($this->getRouteIdentifierPrefix()) . ($redirect ? '?redirect='.$redirect : ''));
     }
-    
+
     /**
      * Delete action for deleting an entity
-     * 
+     *
      * @return \Zend\Http\Response|\Zend\View\Model\ViewModel
      */
     public function deleteAction()
     {
         $id = (int) $this->params()->fromRoute('id', null);
-        
+
         $request = $this->getRequest();
-        
+
         if ($request->isPost()) {
             $confirmed = $request->getPost('confirmed', false);
             if ($confirmed) {
                 $id = (int) $request->getPost('id');
-        
+
                 $this->getService()->delete($id);
-        
+
                 return $this->redirect()->toRoute($this->getRouteIdentifierPrefix());
             }
         }
-        
+
         return new ViewModel(array(
-        
+            'options'  => $this->getOptions(),
         ));
     }
 }
